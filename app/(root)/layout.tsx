83a5effe80b570022d9c934551a868d8e48d1f5f
ArvindKumar6299 <arvindkumar735240@gmail.com>
2024-04-17 10:38:57 +0530

    clerk authentication and sidenav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
+import SideNav from '@/components/shared/SideNav'
 import React from 'react'
 
 const Layout = ({children}:{children: React.ReactNode}) => {
   return (
     <main className='root'>
+        <SideNav/>
         <div className='root-container'>
             <div className='wrapper'>
              {children}

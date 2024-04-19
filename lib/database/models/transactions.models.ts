3a741bd8ef26b1ba0e0a3f60f224cabf7d104da0
ArvindKumar6299 <arvindkumar735240@gmail.com>
2024-04-19 07:19:36 +0530

    model creation done

---
+++ lib/database/models/transactions.models.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, model } from "mongoose";
+import { models } from "mongoose";
+
+
+
+const TransactionSchema = new Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      stripeId: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+      plan: {
+        type: String,
+      },
+      credits: {
+        type: Number,
+      },
+      buyer: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+
+})
+
+const Transaction = models?.Transaction || model('Transaction', TransactionSchema);
+
+export default Transaction;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,31 @@
+//clerkId, email, username, photo, firstName, lastName, planId, creditBalance
+
+import mongoose, { Schema } from "mongoose";
+import { models , model} from "mongoose";
+
+export interface User extends Document {
+    clerkId: string;
+    email: string;
+    username: string;
+    photo?: string;
+    firstName: string;
+    lastName: string;
+    planId?: number;
+    creditBalance: number;
+  }
+
+const UserSchema = new Schema({
+   clerkId:{ type:String, required: true , unique:true},
+   email:{ type:String, required: true , unique:true},
+   username:{ type:String, required: true, unique:true },
+   photo:{ type:String, required: true },
+   firstName:{ type:String,},
+   lastName:{ type:String, },
+   planId:{ type:Number, default:1 },
+   creditBalance:{ type:Number, default:10}
+
+})
+
+const User = models?.User || mongoose.model('User', UserSchema)
+
+export default User;
